24b5b855db9f61873e24939e46400c2465df2a67
Abdul Haseeb <ah7860939@gmail.com>
2023-01-18 14:44:46 -0800

    First Commit

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+error_reporting(0);
+$conn = new mysqli("localhost", "root", "", "hcpms") or die(mysqli_error());
+	$query = $conn->query("SELECT * FROM `user` WHERE `user_id` = '$_SESSION[user_id]'") or die(mysqli_error());
+	$fetch = $query->fetch_array();
+?>
---
+++ name.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(0);
+	require'connect.php';
+	$query = mysqli_query($conn, "SELECT * FROM `user`") or die(mysqli_error());
+	$fetch = mysqli_fetch_array($query);
+	$name = $fetch['name'];
+	
+	
